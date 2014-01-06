1130b4c874de44f3815710c97516da26304c9941
Mike <mcarmody2013@gmail.com>
2014-01-06 17:56:37 -0600

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,33 @@ require 'spec_helper'
 
 describe User do
     
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
+
+    it { should respond_to(:admin) }
+    
+    it { should be_valid }
+    it { should_not be_admin }
+    
+    describe "with admin attribute set to 'true'" do
+        before do
+            @user.save!
+            @user.toggle!(:admin)
+        end
+        
+        it { should be_admin }
+    end
+    
     before do
-        @user = User.new(name: "Example User", email: "user@example.com")
+        @user = User.new(name: "Example User", email: "user@example.com",
+                         password: "foobar", password_confirmation: "foobar")
     end
     
     subject { @user }
     
     it { should respond_to(:name) }
     it { should respond_to(:email) }
-    
-    it { should be_valid }
     
     describe "when email address is already taken" do
         before do
@@ -49,13 +66,8 @@ describe User do
         end
     end
     
-    describe "when email format is valid" do
-        it "should be valid" do
-            addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-            addresses.each do |valid_address|
-                @user.email = valid_address
-                expect(@user).to be_valid
-            end
-        end
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
     end
 end
